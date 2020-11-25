4ae25452fe2a9758680253a9426ee031b16825e2
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2020-11-25 13:00:07 -0500

    added the correct App.js file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/header/header.component";
+import MainComponent from "./components/main/main.component";
+import Footer from "./components/footer/footer.component";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <MainComponent />
+      <Footer />
     </div>
   );
 }
